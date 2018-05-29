31bac980e2cb907415687d8ddeb45af9d34b4c4c
aziz gharbi <agharbi@inovva.com>
2018-05-29 13:12:44 -0400

    auth middleware

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,13 @@
 var HomeController = require('../controller/HomeController');
 var UserController = require('../controller/UserController');
+var Middleware = require("./../middleware/auth");
 
 
 app.get('/',HomeController.index);
 
+app.get("/board",Middleware.auth,function(req,res){
+    res.send('board');
+})
 //auth routes
 app.get('/login',UserController.login);
 app.post('/loginAction',UserController.loginAction);
